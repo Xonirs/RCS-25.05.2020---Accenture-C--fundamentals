aad1c5c930d15eb066feedeb3f44e5553271aa9b
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-06-12 21:46:33 +0300

    day9 complete

---
+++ Day9/Day9_MaxMin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day9_MaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define an int array with at least 5 values. Find the max and min values and print them out.
+            int[] numbers = { 5, 68, 10, 16, 100, 4 };
+            int max = numbers[0];
+            int min = numbers[0];
+
+            //Since we already examined the first element, we can continue from the second
+            //thus, i = 1
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Loop i value: {i}, current array element: {numbers[i]}, previous max: {max}");
+
+                //if the current number in array is greater than the previous max
+                if (numbers[i] > max)
+                {
+                    // we change it
+                    max = numbers[i];
+                }
+
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"Max:{max}, Min:{min}");
+        }
+    }
+}
---
+++ Day9/Day9_randomArrayTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day9_randomArrayTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define an int array with size of 10.
+            var numberArray = new int[10];
+
+            //Assign all the elements with Random values. 
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                Random random = new Random();
+                int a = random.Next(100); //random values until 100
+                numberArray[i] = a;
+            }
+
+            //In a new loop, print all the values.
+            foreach (int number in numberArray)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}

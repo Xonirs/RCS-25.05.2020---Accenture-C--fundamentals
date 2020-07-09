3246974ee67409f1db5662d8f9f1528078bb94dc
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-07-09 19:34:36 +0300

    day18 exception p3

--- Day18/Day18_Exceptions/Program.cs
+++ Day18/Day18_Exceptions/Program.cs
@@ -11,7 +11,15 @@ namespace Day18_Exceptions
         //  2>or handle them
         static void Main(string[] args)
         {
-            ExceptionTest4();
+            ExceptionTest7();
+            //try
+            //{
+            //    ExceptionTest6();
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Something went wrong in one of the ExceptionTest methods!");
+            //}
         }
 
         public static void ExceptionTest1()
@@ -69,7 +77,6 @@ namespace Day18_Exceptions
                 Console.WriteLine($"Exception message: {e.Message}");
             }
         }
-
         public static void ExceptionTest4()
         {
             int n1 = 100;
@@ -88,6 +95,96 @@ namespace Day18_Exceptions
                 Console.WriteLine("Something went wrong");
                 Console.WriteLine($"Values:{n1}, {n2}");
                 Console.WriteLine($"Exception message: {e.Message}");
+            }
+            finally
+            {
+                //Block lets you execute code after try..catch, regardless of the result. 
+                //It will execute always, either there was an exception or there were no exceptions
+                Console.WriteLine("Finished with our try..catch!");
+            }
+        }
+        public static void ExceptionTest5()
+        {
+            int n1 = 100;
+            int n2 = 0;
+
+            try
+            {
+                //Block where we are going to try to execute some code                
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                //Block where we are going to handle exceptions/errors/problems/etc.
+                //e - exception variable name
+                Console.WriteLine("Something went wrong");
+                Console.WriteLine($"Values:{n1}, {n2}");
+                Console.WriteLine($"Exception message: {e.Message}");
+                //throw allows us to re-create the SAME exception
+                //as if a "new" exception happened once more
+                throw; 
+            }
+            finally
+            {
+                //Block lets you execute code after try..catch, regardless of the result. 
+                //It will execute always, either there was an exception or there were no exceptions
+                Console.WriteLine("Finished with our try..catch!");
+            }
+
+            if (true)
+            {
+                //...
+            }
+
+            Console.WriteLine("Method ended!");
+        }
+
+        public static void ExceptionTest6()
+        {
+            int[] numbers = { 1, 2, 3, 10 };
+            int position = 10;
+
+            try
+            {
+                Console.WriteLine(numbers[position]);
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine($"Please use an index inside the range of 0..{numbers.Length - 1}");
+                //Console.WriteLine($"There was an exception: {e.Message}");
+            }
+        }
+
+        public static void ExceptionTest7()
+        {
+            int[] numbers = { 1, 2, 3, 10 };
+            int position = 3, number = 3;
+
+            try
+            {
+                Console.WriteLine(numbers[position]);
+                var result = numbers[position] / number;
+                Console.WriteLine(result);
+                TestClass test = null;
+                test.Name = "Test name value";
+                Console.WriteLine(test.Name);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine($"Please use an index inside the range of 0..{numbers.Length - 1}");
+                //Console.WriteLine($"There was an exception: {e.Message}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Oops, somehow we tried to divide by zero, sorry!");
+                //Console.WriteLine($"There was an exception: {e.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something wrong unexpected happened!");
+                Console.WriteLine($"Here is the message: {ex.Message}");
             }
         }
     }
---
+++ Day18/Day18_Exceptions/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day18_Exceptions
+{
+    class TestClass
+    {
+        public string Name { get; set; }
+    }
+}

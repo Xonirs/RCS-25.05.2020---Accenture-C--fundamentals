4f85f7ac5e0cec9ab1007845336c9aadba9b65b1
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-07-15 21:34:40 +0300

    day 21 complete

---
+++ Day21/Day21_Abstract_Shape/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Day21_Abstract_Shape
+{
+    abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    class Square : Shape
+    {
+        private int edge = 0;
+        public Square(int edgeSize)
+        {
+            edge = edgeSize;        
+        }
+        public override double Area()
+        {
+            return edge * edge;
+        }
+    }
+
+    class Circle : Shape
+    {
+        private int radius = 0;
+        public Circle(int radiusSize)
+        {
+            radius = radiusSize;
+        }
+        public override double Area()
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(25);
+            Console.WriteLine($"Square area is: {square.Area()}");
+            Circle circle = new Circle(25);
+            Console.WriteLine($"Circle area is: {circle.Area()}");
+        }
+    }
+}
--- Day21/Day21_Abstraction_Animal/Program.cs
+++ Day21/Day21_Abstraction_Animal/Program.cs
@@ -2,35 +2,80 @@
 
 namespace Day21_Abstraction_Animal
 {
-    class Animal
+    //abstract class - a restricted class that cannot be used to create objects. to use it, you must inherit from it.
+    abstract class Animal
     {
-        public virtual void AnimalSound() { Console.WriteLine("This is generic animal sound."); }
+        //abstract method - can only be used in an abstract class. it does not have a body, just a method signature.
+        //                  The method body MUST be provided by the derived class.
+        public abstract void AnimalSound();
+        public void Sleep() 
+        { 
+            Console.WriteLine("Zzz"); 
+        }
+        public void ExecuteDay()
+        {
+            Sleep();
+            Console.WriteLine("Animal is waking up");
+            for (int i = 0; i < 3; i++)
+            {
+                AnimalSound();
+            }
+            Sleep();
+        }
+        public string Name { get; set; }
     }
 
     class Dog : Animal
     {
-        public void SniffPeople() { Console.WriteLine("Sniffing people right now!!"); }
-        public override void AnimalSound() { Console.WriteLine("WOOF-WOOF!"); }
+        public void SniffPeople() 
+        { 
+            Console.WriteLine("Sniffing people right now!!"); 
+        }
+        //since AnimalSound() is abstract in base class, we MUST override it and provide implementation
+        public override void AnimalSound() 
+        { 
+            Console.WriteLine("WOOF-WOOF!"); 
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void ScratchPeople() 
+        { 
+            Console.WriteLine("Scratching people right now!!"); 
+        }
+        public override void AnimalSound() 
+        { 
+            Console.WriteLine("MEOW-MEOW!"); 
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Animal animal = new Animal(); animal.AnimalSound();
-            Dog dog = new Dog(); dog.SniffPeople(); dog.AnimalSound();
-            Animal dog2 = new Dog(); dog2.AnimalSound();
-            Console.WriteLine();
-            DoAnimalStuff(animal);
-            DoAnimalStuff(dog);
-            DoAnimalStuff(dog2);
+            string temporaryText = "Blah blah blah";
+            int tempNumber = 133;
+            Dog dog = new Dog();
+            dog.Name = "Fluffy";
+            dog.AnimalSound();
+            Cat cat = new Cat();
+            cat.Name = "Buffy";
+            cat.AnimalSound();
+            Console.WriteLine(); 
+            DoAnimalStuff(dog); 
+            DoAnimalStuff(cat);
+            Console.WriteLine(); 
+            cat.Sleep(); 
+            dog.Sleep();
+            Console.WriteLine(); 
+            dog.ExecuteDay();
         }
+
         static void DoAnimalStuff(Animal animal)
         {
             animal.AnimalSound();
-            //animal.OtherMethod1();
-            //animal.OtherMethod2();
-            //animal.OtherMethod3();
+            animal.Sleep();
         }
     }
 }

e02745ac0c61aad48397efaf0bf89cec9d818041
Xonirs <66732623+Xonirs@users.noreply.github.com>
2020-07-16 19:50:52 +0300

    day 22 interface p1

---
+++ Day22/Day21_Abstraction_Recap/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day21_Abstraction_Recap
+{
+    abstract class Vehicle
+    {
+        public bool HasFuel { get; set; }
+        public string Color { get; set; }
+        public void GoForward() 
+        {
+            if (HasFuel == false) //!HasFuel
+            {
+                FillFuel();
+            }
+            Console.WriteLine("Vehicle is moving forward"); 
+        }
+        public abstract void FillFuel();
+    }
+    class Car : Vehicle
+    {
+        public bool HasChildrenSeats { get; set; }
+        public override void FillFuel() { Console.WriteLine("Simulation car fuel filling"); }
+    }
+    class Boat : Vehicle
+    {
+        public bool SailIsSet { get; set; }
+        public override void FillFuel() { Console.WriteLine("Simulation boat fuel filling"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(); car.Color = "black"; car.GoForward();
+            Boat boat = new Boat(); boat.Color = "red"; boat.GoForward();
+            car.HasChildrenSeats = true;
+            boat.SailIsSet = false;
+            car.HasFuel = true;
+            Console.WriteLine(); car.GoForward(); 
+        }
+    }
+}
---
+++ Day22/Day22_Interface/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day22_Interface
+{
+    //interface - is a completely "abstract class", meaning it only contains abstract members (methods, properties)
+    //            is just a contract, just a list of features with no implemenations whatsoever
+    //a class can inherit only a single other class (be it abstract or regular), 
+    //but a class can inherit multiple interfaces
+
+    //by default, all of the members for interface are abstract and public
+    interface IAnimal
+    {
+        int LegCount { get; set; }
+        void AnimalSound();
+    }
+
+    interface IBird
+    {
+        int WingCount { get; set; }
+        void StartFlying();
+    }
+
+    class Pig : IAnimal
+    {
+        public int LegCount { get; set; }
+
+        public void AnimalSound()
+        {
+            Console.WriteLine("The pig says OINK OINK");
+        }
+    }
+
+    class Eagle : IAnimal, IBird
+    {
+        public int LegCount { get; set; }
+        public int WingCount { get; set; }
+
+        public void AnimalSound()
+        {
+            Console.WriteLine("The bird says tweet tweet");
+        }
+
+        public void StartFlying()
+        {
+            Console.WriteLine("The bird starts flying");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig pig = new Pig();
+            pig.LegCount = 4;
+            pig.AnimalSound();
+            Eagle eagle = new Eagle();
+            eagle.LegCount = 2;
+            eagle.WingCount = 2;
+            eagle.AnimalSound();
+            eagle.StartFlying();
+            Console.WriteLine();
+
+            List<IAnimal> animals = new List<IAnimal>();
+            animals.Add(pig);
+            animals.Add(eagle);
+            foreach (IAnimal animal in animals)
+            {
+                animal.AnimalSound();
+            }
+        }
+    }
+}
